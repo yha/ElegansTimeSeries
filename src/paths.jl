699e4cc4328f20f1c08d1c059770a531c74af2b0
yha <yharel@campus.technion.ac.il>
2024-02-19 17:24:54 +0200

    Paths from toml file. DE-PCA filename funcs

--- src/paths.jl
+++ src/paths.jl
@@ -1,66 +1,48 @@
 using JLD2, FileIO
-"""
-A somewhat hacky way to load correct paths for various machines
-"""
-paths = let hostname = gethostname()
-    if hostname == "bi-sternlab21"
-        coord_stats = "H:/coord-stats-binned"
-        video = "U:/experiments/"
-        contours = "U:/cached-data/contours"
-        midpoints = "F:/cached-data/midpoints"
-        pop_des = "E:/cached-data/DEs"
-        depca = "H:/cached-data/DE-PCA"
-        
-    elseif hostname == "bi-sternlab33"
-        coord_stats = "U:/cached-data/coord-stats-binned"
-        video = "U:/experiments/"
-        contours = "U:/cached-data/contours"
-        midpoints = "U:/cached-data/midpoints"
-        pop_des = "U:/cached-data"
-        depca = "U:/cached-data/DE-PCA"
+using TOML
 
-    elseif hostname == "BI-Sternlab3"
-        coord_stats = "U:/cached-data/coord-stats-binned"
-        video = "U:/experiments/"
-        contours = "U:/cached-data/contours"
-        midpoints = "F:/midpoints"
-        pop_des = "F:/cached-data/DEs"
-        depca = "F:/cached-data/DE-PCA"
+function data_paths(; expected_params = ["video", "contours", "midpoints", "area", "depca", "depca_individual", "depca_bs", "stages"])
+	paths_dict = TOML.parsefile("$(@__DIR__)/../paths.toml")["paths"]
+	@assert keys(paths_dict) == Set(expected_params)
 
-    else
-        coord_stats = "../../coord-stats-binned"
-        video =  "$(homedir())/experiments/"
-        contours = "$(homedir())/cached-data/contours"
-        midpoints = "$(homedir())/cached-data/midpoints"
-        pop_des = "$(homedir())/cached-data/DEs"
-        depca = "$(homedir())/cached-data/DE-PCA"        
-        area = "$(homedir())/cached-data"
-    end
-
-    depca_individual = "$depca/individuals"
-    depca_bs = "$depca/bootstraps"
-    (; #shape_base, shape, 
-        depca, depca_individual, depca_bs,
-        coord_stats, video, contours, midpoints, pop_des
-        )
+	NamedTuple((Symbol(k), normpath(joinpath(@__DIR__, v))) for (k,v) in paths_dict)
 end
+
+paths = data_paths()
 
 function load_depcas(source)
 	d = load(source)
 	pcas = d["pcas"]
-	conds = d["conditions"]
 	indices = d["de_indices"]
-	wells = d["wells"]
+	if haskey(d, "conditions")
+		conds = d["conditions"]
+		wells = d["wells"]
+	else
+		# Sanitized data format: 
+		# single condition specified as strain& DS, and well count in each experiment rather than well list
+		strain, ds = d["strain"], d["DS"]
+		n_individuals = d["n_individuals"] # vector of pairs of (experiment, n)
+		conds = [(strain, ds)]
+		wells = [(; experiment, well = "well $i") for (experiment,n) in n_individuals for i = 1:n]
+	end
 	@info "Loaded $(length(pcas)) PCAs for $(join(join.(conds, "-"), ", ")) (n=$(length(wells)))"
 	(; pcas, indices, conds, wells)
 end
-function make_depca_cache()
-    #cache = LRU(; maxsize = 20)
-    cache = Dict{String,Any}()
+
+function make_depca_cache(; cache = Dict{String, Any}(), path = data_paths().depca)
     return get_depcas!(source) = get!(cache, source) do
         @info "Loading PCAs from `$source`"
-        res = load_depcas(joinpath(paths.depca, source))
-        #@assert length(res.pcas) == nbins_total
-        #res
+        load_depcas(joinpath(path, source))
     end
 end
+
+meta_string(params) = meta_string(; params...)
+meta_string(; nwells, ntrim, pca_winlen, confidence_threshold) = 
+	"n=$nwells, n_trim=$ntrim, windowlength=$pca_winlen, conf_th=$confidence_threshold"
+
+depca_filename(depca_params) = "DE-PCA $(meta_string(depca_params)).jld2"
+depca_vars_filename(depca_params)  = "DE-PCA vars $(meta_string(depca_params)).jld2"
+depca_cv_filename(nwindows, nsamples, depca_params) = 
+	"CV PCA errors, $nwindows windows, $nsamples sample, $(meta_string(depca_params)).jld2" 
+depca_individual_filename(well, maxoutdim, depca_params) = 
+	"DE-PCA $(well.experiment) $(well.well), $(meta_string(depca_params)), maxoutdim=$maxoutdim.jld2"
