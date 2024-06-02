4e29e16fa62108dccac555bcc7f82637a1a287ee
yha <yharel@campus.technion.ac.il>
2024-06-02 15:32:26 +0300

    Sanitization script updates

--- scripts/make-renamed-well-data.jl
+++ scripts/make-renamed-well-data.jl
@@ -5,7 +5,8 @@ Coordinates, midlines and individuals' DE-PCAs are simply copied to new names, p
 are also re-saved with the names of wells omitted.
 Also saves the mapping from lab-local name to sanitized names/numerals in CSV files.
 
-Input midpoints directory should have a subdirectory per condition.
+Input midpoints directory should have a subdirectory per condition, and should only include
+wells to be exported.
 """
 
 using CSV, DataFrames
@@ -25,7 +26,7 @@ out_path         = "G:/sanitized/data"
 out_path_mapping = "G:/sanitized"
 
 coordinate_paths = ["U:/experiments/reemy", "U:/experiments/manal/Coords"]
-ex_list_files = "$(@__DIR__)/../" .* ["exp-list-filtered-RA.csv", "exp-list-filtered-NG.csv"]
+ex_list_files = "$(@__DIR__)/../" .* ["exp-list-filtered-RA.csv", "exp-list-filtered-NG-no-N2.csv"]
 
 ##
 
@@ -36,7 +37,7 @@ contour_methods = [Thresholding(1.0,0.35), Thresholding(1.0,0.34)]
 
 ##
 
-ex_dfs = [DataFrame(CSV.File(f)) for f in ex_list_files]
+ex_dfs = [DataFrame(CSV.File(f; stringtype = String)) for f in ex_list_files]
 exs_from_lists = DataFrame(
     name   = reduce(vcat, df.name for df in ex_dfs),
     strain = reduce(vcat, df.strain for df in ex_dfs),
@@ -110,7 +111,7 @@ let
     only_midfile_exs   = Vector{String}(setdiff(exs_with_midfiles.experiment, exs_from_lists.name))
     i = exs_with_midfiles.experiment .∈ Ref(only_midfile_exs)
     only_midfile_conds = [(; r.strain, r.DS) for r in eachrow(exs_with_midfiles[i,:])]
-    isempty(only_midfile_exs) || @info """
+    isempty(only_midfile_exs) || @error """
         Experiments with midpoint files not in experiment list: $(join(("$ex ($(condname(cond)))" for (ex, cond) in zip(only_midfile_exs, only_midfile_conds)), ", "))
     """
 end
@@ -131,6 +132,12 @@ let depca_wells_from_notebook = CSV.read(well_list_from_notebook_path, DataFrame
 end
 ##
 
+# # listing differences between lists if assertion above fails:
+# depca_wells_from_notebook = CSV.read(well_list_from_notebook_path, DataFrame)
+# antijoin(depca_wells_from_notebook, wells_with_midfiles_df; on = [:experiment, :well])
+# antijoin(wells_with_midfiles_df, depca_wells_from_notebook; on = [:experiment, :well])
+# ##
+
 ex_name_map = mapreduce(vcat, cond2exs) do (cond, exs)
     map(enumerate(exs)) do (i, ex)
         #ex => "$(condname(cond)), experiment $i"
@@ -139,6 +146,11 @@ ex_name_map = mapreduce(vcat, cond2exs) do (cond, exs)
 end |> OrderedDict
 
 CSV.write("$out_path_mapping/ex-name-mapping.csv", [(; experiment=ex, sanitized=s) for (ex,s) in ex_name_map])
+
+# # same as older saved mapping?
+# DataFrame((; experiment=ex, sanitized=s) for (ex,s) in ex_name_map) == 
+# DataFrame(CSV.File("U:/cached-data/sanitized/ex-name-mapping.csv"))
+
 ##
 
 # map each well to a unique index within its experiment
@@ -151,6 +163,12 @@ well_index_map = mapreduce(vcat, cond2exs) do (cond, exs)
 end |> OrderedDict
 
 CSV.write("$out_path_mapping/well-indexes.csv", [(; w.experiment, w.well, index) for (w, index) in well_index_map])
+
+##
+
+# # same as older saved mapping?
+# DataFrame((; w.experiment, w.well, index) for (w, index) in well_index_map) == 
+# DataFrame(CSV.File("U:/cached-data/sanitized/well-indexes.csv"))
 
 ##
 
@@ -168,7 +186,7 @@ _cp(source, dest) = (@assert isfile(source); println(dest))
 
 
 
-individual_depca_filenames = readdir(individual_depca_path)
+# individual_depca_filenames = readdir(individual_depca_path)
 
 @progress "conditions" for cond in conditions
     #cond_path = "$midpoints_path/$(condname(cond))"
@@ -238,30 +256,31 @@ end
 # Suffix expected on all DE-PCA files, based on params that were used in the pipepline 
 depca_params_suffix = "n_trim=1, windowlength=30, conf_th=0.05"
 @assert Set(conditions) == keys(cond2wells)
-depca_filename(cond) = "DE-PCA $(condname_full(cond)), n=$(length(cond2wells[cond])), $depca_params_suffix.jld2"
+depca_suffix(cond) = "$(condname_full(cond)), n=$(length(cond2wells[cond])), $depca_params_suffix.jld2"
+depca_filename(cond) = "DE-PCA " * depca_suffix(cond)
+depca_vars_filename(cond) = "DE-PCA vars " * depca_suffix(cond)
 depca_file_path(cond) = joinpath(depca_path, depca_filename(cond))
+depca_vars_file_path(cond) = joinpath(depca_path, depca_vars_filename(cond))
 
 for cond in conditions
     @assert isfile(depca_file_path(cond))
 end
 
-
 ##
 
-depca_out_path = "$out_path/DE-PCAs"
-mkpath(depca_out_path)
+function well_list_to_exs_and_counts(wells)
+    experiments = unique(w.experiment for w in wells)
+    [ex_name_map[ex] => count(w.experiment == ex for w in wells) for ex in experiments]
+end
 
-@progress for cond in conditions
-    @info condname(cond)
+function sanitize_depca_file(src, dest; strain, DS)
+    if ispath(dest)
+        @info "Skipping existing output file: $dest"
+        return
+    end
+    @info "Loading `$src`"
+    d = load(src)
 
-    path = depca_file_path(cond)
-    dest = "$depca_out_path/$(depca_filename(cond))"
-    isfile(dest) && continue
-
-    @info "Loading `$path`"
-    d = load(path)
-
-    (; strain, DS) = cond
     @assert (strain, DS) == only(d["conditions"])
 
     de_indices, wells, pcas = d["de_indices"], d["wells"], d["pcas"]
@@ -269,12 +288,109 @@ mkpath(depca_out_path)
     @assert wells == sort(cond2wells[(;strain, DS)])
     # In the sanitized data, store sanitized experiment names and a count of wells in each experiment,
     # instead of well list
-    experiments = unique(w.experiment for w in wells)
-    n_individuals = [ex_name_map[ex] => count(w.experiment == ex for w in wells) for ex in experiments]
+    n_individuals = well_list_to_exs_and_counts(wells)
 
     @assert sum( n for (_,n) in n_individuals ) == length(wells) == size( d["de_indices"], 2 )
 
     @info "Saving to `$dest`"
     jldsave(dest; strain, DS, n_individuals, de_indices, pcas)
+end
+
+function sanitize_vars_file(src, dest; strain, DS, is_population = false)
+    if ispath(dest)
+        @info "Skipping existing output file: $dest"
+        return
+    end
+    @info "Loading `$src`"
+    d = load(src)
+
+    # variance files also have an "nwindows" field, but currently it has wrong
+    # data in most files, and it's not used anyway
+    wells, tvars, vars = d["wells"], d["tvars"], d["vars"]
+    @assert issorted(wells)
+    n_individuals = well_list_to_exs_and_counts(wells)
+
+    if !is_population
+        @assert sum( n for (_,n) in n_individuals ) == length(wells) == size(vars,1) == size(tvars,1)
+    end
+
+    @info "Saving to `$dest`"
+    jldsave(dest; strain, DS, n_individuals, vars, tvars)
+end
+
+
+depca_out_path = "$out_path/DE-PCAs"
+mkpath(depca_out_path)
+
+@progress for cond in conditions
+    @info condname(cond)
+    (; strain, DS) = cond
+
+    # DE-PCA file
+    let
+        src = depca_file_path(cond)
+        dest = "$depca_out_path/$(depca_filename(cond))"
+        sanitize_depca_file(src, dest; strain, DS)
+    end
+
+    # Variances file
+    let
+        vars_path = depca_vars_file_path(cond)
+        vars_dest = "$depca_out_path/$(depca_vars_filename(cond))"
+        sanitize_vars_file(vars_path, vars_dest; strain, DS)
+    end
+end
+
+## extra hard-coded files
+
+population_var_files = [("DE-PCA population vars all bins N2 0DS, 10000 samples per bin, n=123, n_trim=1, windowlength=30, conf_th=0.05.jld2",
+"N2", 0)
+]
+
+for (file, strain, DS) in population_var_files
+    src = joinpath(depca_path, file)
+    dest = joinpath(depca_out_path, file)
+    sanitize_vars_file(src, dest; strain, DS, is_population = true)
+end
+
+
+extra_depca_files = [("DE-PCA N2 0DS, n=123, n_trim=1, windowlength=15, conf_th=0.05.jld2", "N2", 0),
+                     ("DE-PCA N2 0DS, n=123, n_trim=1, windowlength=60, conf_th=0.05.jld2", "N2", 0),
+                     ("DE-PCA N2 0DS, n=123, n_trim=1, windowlength=120, conf_th=0.05.jld2", "N2", 0)
+]
+
+for (file, strain, DS) in extra_depca_files
+    src = joinpath(depca_path, file)
+    dest = joinpath(depca_out_path, file)
+    sanitize_depca_file(src, dest; strain, DS)
+end
+
+extra_vars_files =  [
+                    # different window lengths
+                    ("DE-PCA vars N2 0DS, n=123, n_trim=1, windowlength=15, conf_th=0.05.jld2", "N2", 0),
+                    ("DE-PCA vars N2 0DS, n=123, n_trim=1, windowlength=60, conf_th=0.05.jld2", "N2", 0),
+                    ("DE-PCA vars N2 0DS, n=123, n_trim=1, windowlength=120, conf_th=0.05.jld2", "N2", 0),
+                    # individual's variances in each bin in population's "joint PCA" (PCA across all time bins)
+                    ("DE-PCA vars all bins N2 0DS, 10000 samples per bin, n=123, n_trim=1, windowlength=30, conf_th=0.05.jld2", "N2", 0)
+]
+
+for (file, strain, DS) in extra_vars_files
+    src = joinpath(depca_path, file)
+    dest = joinpath(depca_out_path, file)
+    sanitize_vars_file(src, dest; strain, DS)
+end
+
+##
+
+# files that don't need sanitizing
+extra_to_copy = ["DE-PCA all bins N2 0DS, 10000 samples per bin, n=123, n_trim=1, windowlength=30, conf_th=0.05.jld2"]
+
+for file in extra_to_copy
+    src = joinpath(depca_path, file)
+    dest = joinpath(depca_out_path, file)
+    if !ispath(dest)
+        @info "Copying" src dest
+        cp(src, dest)
+    end
 end
 
